e6797e3e37ab5c178aa3c868f954efe0b0914aea
iamEtornam <sunumacbright@gmail.com>
2024-01-21 09:36:46 +0000

    first commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Etornam on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
